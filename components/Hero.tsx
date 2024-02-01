6cff202f54c7374b26e61ce377a6694b71c87c31
tahaamin2013 <tahaamindob2013@gmail.com>
2024-02-02 02:23:43 +0500

    SDFFFFFFFFFFF

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -49,8 +49,6 @@ export default async function Hero() {
           </div>
         </div>
       </div>
-
-      
     </section>
   );
 }
--- components/Providers.tsx
+++ components/Providers.tsx
@@ -9,10 +9,10 @@ export default function CartProvider({ children }: { children: ReactNode }) {
       mode="payment"
       cartMode="client-only"
       stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
-      successUrl="https://commerce-next-yt.vercel.app/stripe/success"
-      cancelUrl="https://commerce-next-yt.vercel.app/stripe/error"
+      successUrl="http://localhost:3000/success"
+      cancelUrl="http://localhost:3000/error"
       currency="USD"
-      billingAddressCollection={false}
+      billingAddressCollection={true}
       shouldPersist={true}
       language="en-US"
     >
